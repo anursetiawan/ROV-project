de3a37e1e4334b11df049cdb459d73de4e37d140
Aprianto Nursetiawan <noersetiawan@gmail.com>
2017-09-23 15:09:08 +0700

    Added current working code

---
+++ NanoReceiverSerial/sketch_jul21bNanoReceiverSerial.ino
@@ -0,0 +1,397 @@
+// setting up variables
+int keyInput;       // variable for serial input
+char* horizontalState; // state of the horizontal motors
+char* verticalState; // state of the vertical motors
+int lampState; //state of the lamp
+
+/* motor configuration is as follow:
+ * motor A is the starboard top side, connected to motor driver I on A side
+ * motor B is the port top side, connected to motor driver I on B side
+ * motor C is the starboard bottom side, connected to motor driver II on A side
+ * motor D is the port bottom side, connected to motor driver II on B side
+ * motor driver I is on starboard side, motor driver II is on port side
+ */
+
+// this block is for motor driver I
+const int pwmA = 6; // ENA pin
+const int pwmB = 9; // ENB pin
+const int inA1 = 2; // IN1 pin
+const int inA2 = 3; // IN2 pin
+const int inB3 = 4; // IN3 pin
+const int inB4 = 5; // IN4 pin
+
+// this block is for motor driver II
+const int pwmC = 10; // ENA pin
+const int pwmD = 11; // ENB pin
+const int inC1 = 14; // IN1 pin
+const int inC2 = 15; // IN2 pin
+const int inD3 = 16; // IN3 pin
+const int inD4 = 17; // IN4 pin
+
+// lamp toggle, connected with a relay
+const int lamp = 13; 
+
+void setup() {                    // put your setup code here, to run once:
+
+Serial.begin(2000000); // serial baud rate, make sure this is the same with Serial Monitor
+Serial.setTimeout(1); // gotta go fast
+
+// this block is for motor driver I
+pinMode(pwmA, OUTPUT);
+pinMode(pwmB, OUTPUT);
+pinMode(inA1, OUTPUT);
+pinMode(inA2, OUTPUT);
+pinMode(inB3, OUTPUT);
+pinMode(inB4, OUTPUT);
+
+digitalWrite(inA1, LOW);
+digitalWrite(inA2, LOW);
+digitalWrite(inB3, LOW);
+digitalWrite(inB4, LOW);
+analogWrite(pwmA, 0);
+analogWrite(pwmB, 0);
+
+// this block is for motor driver II
+pinMode(pwmC, OUTPUT);
+pinMode(pwmD, OUTPUT);
+pinMode(inC1, OUTPUT);
+pinMode(inC2, OUTPUT);
+pinMode(inD3, OUTPUT);
+pinMode(inD4, OUTPUT);
+
+digitalWrite(inC1, LOW);
+digitalWrite(inC2, LOW);
+digitalWrite(inD3, LOW);
+digitalWrite(inD4, LOW);
+analogWrite(pwmC, 0);
+analogWrite(pwmD, 0);
+
+// this block is for lamp trigger
+pinMode(lamp, OUTPUT);
+
+digitalWrite(lamp, LOW);
+
+/* all output pins are set as OUTPUT and LOW
+ * each run as to ensure there is no unexpected behavior 
+ */
+
+Serial.println("READY");
+Serial.println("-----");
+delay(100);
+mStop();
+motorState();
+}
+
+void loop() {                     // put your main code here, to run repeatedly:
+  receiveOneCharacter();
+  conditionsCase();
+}
+
+void conditionsCase(){                // this block contain list of maneuvers
+  switch (keyInput){
+    case '0':         // stopping all motors
+      mStop();
+      break;
+  // ===      
+    case 'q':         // this block is for motor driver I
+      mForwardLeft();
+      break;
+    case 'w':
+      mForward();
+      break;
+    case 'e':
+      mForwardRight();
+      break;
+  // ---
+    case 'a':
+      mYawLeft();
+      break;
+    case 's':
+      mStopH();
+      break;
+    case 'd':
+      mYawRight();
+      break;
+  // ---
+    case 'z':
+      mBackwardLeft();
+      break;
+    case 'x':
+      mBackward();
+      break;
+    case 'c':
+      mBackwardRight();
+      break;
+  // ===
+    case 'i':           // this block is for motor driver II
+      mUp();
+      break;
+  // ---
+    case 'j':
+      mRollLeft();
+      break;
+    case 'k':
+      mStopV();
+      break;
+    case 'l':
+      mRollRight();
+      break;
+  // ---
+    case ',':
+      mDown();
+      break;
+  // ===
+    case '1':         // this is for the lamp, it is number 1 (one) by the way, not an l (L)
+      lampOn();
+      break;
+    case '2':
+      lampOff();
+      break; 
+}
+}
+
+void receiveOneCharacter(){       // reading the input from serial monitor, can only parse the first character sent
+  keyInput = Serial.read();
+
+}
+
+void motorState(){                // displaying the state of all outputs and maneuver configuration, useful for debugging
+  Serial.print(horizontalState);  // result is displayed in format:
+  Serial.print(" | ");            // H motor manv | V motors manv   [pwmA] inA1 inA2 inB3 inB4 [pwmB]|[pwmC] inC1 inC2 inD3 inD4 [pwmD]
+  Serial.print(verticalState);    // example:
+  // ---                          // Forward | Down   [1]1010[1]|[1]0101[1]
+  Serial.print("   ");
+  // ---
+  Serial.print("[");
+  Serial.print(bitRead(PIND,6));
+  Serial.print("]");
+  Serial.print(bitRead(PIND,2));
+  Serial.print(bitRead(PIND,3));
+  Serial.print(bitRead(PIND,4));
+  Serial.print(bitRead(PIND,5));
+  Serial.print("[");
+  Serial.print(bitRead(PINB,1));
+  Serial.print("]");
+  // ---
+  Serial.print(" | ");
+  // ---
+  Serial.print("[");
+  Serial.print(bitRead(PINB,2));
+  Serial.print("]");
+  Serial.print(bitRead(PINC,0));
+  Serial.print(bitRead(PINC,1));
+  Serial.print(bitRead(PINC,2));
+  Serial.print(bitRead(PINC,3));
+  Serial.print("[");
+  Serial.print(bitRead(PINB,3));
+  Serial.println("]");
+  Serial.println("-----");
+}
+
+// ===
+
+void mStop(){                     // stopping all motors
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 0);
+  analogWrite(pwmB, 0);
+  horizontalState = "Stop";
+  digitalWrite(inC1, LOW);
+  digitalWrite(inC2, LOW);
+  digitalWrite(inD3, LOW);
+  digitalWrite(inD4, LOW);
+  analogWrite(pwmC, 0);
+  analogWrite(pwmD, 0);
+  verticalState = "Stop";
+}
+
+// ===
+
+void mForwardLeft(){              // only the horizontal starboard motor is pushing forward
+  digitalWrite(inA1, HIGH);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Forward Left";
+  motorState();
+}
+
+void mForward(){                  // both horizontal motors are pushing forward
+  digitalWrite(inA1, HIGH);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, HIGH);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Forward";
+  motorState();
+}
+
+void mForwardRight(){             // only the horizontal port motor is pushing forward
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, HIGH);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Forward Right";
+  motorState();
+}
+
+void mYawLeft(){                  // both horizontal motors are rotating against each other, ROV is heading towards left
+  digitalWrite(inA1, HIGH);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, HIGH);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Yaw Left";
+  motorState();
+}
+
+void mStopH(){                    // stopping all horizontal motors
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 0);
+  analogWrite(pwmB, 0);
+  horizontalState = "Stop";
+  motorState();
+}
+
+void mYawRight(){                 // both horizontal motors are rotating against each other, ROV is heading towards right
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, HIGH);
+  digitalWrite(inB3, HIGH);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Yaw Right";
+  motorState();
+}
+
+void mBackwardLeft(){             // only the horizontal starboard motor is pushing backward
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, HIGH);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, LOW);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Backward Left";
+  motorState();
+}
+
+void mBackward(){                 // both horizontal motors are pushing backward
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, HIGH);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, HIGH);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Backward";
+  motorState();
+}
+
+void mBackwardRight(){            // only the horizontal port motor is pushing backward
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB3, LOW);
+  digitalWrite(inB4, HIGH);
+  analogWrite(pwmA, 255);
+  analogWrite(pwmB, 255);
+  horizontalState = "Backward Right";
+  motorState();
+}
+
+// ===
+
+void mUp(){                       // both vertical motors are pushing upward
+  digitalWrite(inC1, HIGH);
+  digitalWrite(inC2, LOW);
+  digitalWrite(inD3, HIGH);
+  digitalWrite(inD4, LOW);
+  analogWrite(pwmC, 255);
+  analogWrite(pwmD, 255);
+  verticalState = "Up";
+  motorState();
+}
+
+void mRollLeft(){                 // both vertical motors are rotating against each other, ROV is banking left
+  digitalWrite(inC1, HIGH);
+  digitalWrite(inC2, LOW);
+  digitalWrite(inD3, LOW);
+  digitalWrite(inD4, HIGH);
+  analogWrite(pwmC, 255);
+  analogWrite(pwmD, 255);
+  verticalState = "Roll Left";
+  motorState();
+}
+
+void mStopV(){                    // stopping all vertical motors
+  digitalWrite(inC1, LOW);
+  digitalWrite(inC2, LOW);
+  digitalWrite(inD3, LOW);
+  digitalWrite(inD4, LOW);
+  analogWrite(pwmC, 0);
+  analogWrite(pwmD, 0);
+  verticalState = "Stop";
+  motorState();
+}
+
+void mRollRight(){                // both vertical motors are rotating against each other, ROV is banking right
+  digitalWrite(inC1, LOW);
+  digitalWrite(inC2, HIGH);
+  digitalWrite(inD3, HIGH);
+  digitalWrite(inD4, LOW);
+  analogWrite(pwmC, 255);
+  analogWrite(pwmD, 255);
+  verticalState = "Roll Right";
+  motorState();
+}
+
+void mDown(){                     // both vertical motors are pushing downward
+  digitalWrite(inC1, LOW);
+  digitalWrite(inC2, HIGH);
+  digitalWrite(inD3, LOW);
+  digitalWrite(inD4, HIGH);
+  analogWrite(pwmC, 255);
+  analogWrite(pwmD, 255);
+  verticalState = "Down";
+  motorState();
+}
+
+// ===
+
+void lampOn(){
+  digitalWrite(lamp, HIGH);
+  Serial.println("Lamp is ON");
+  Serial.println("-----");
+}
+
+void lampOff(){
+  digitalWrite(lamp, LOW);
+  Serial.println("Lamp is OFF");
+  Serial.println("-----");
+}
+
+/*
+void lampTrigger(){               // lamp toggle
+  lampState = digitalRead(lamp);
+
+  if (lampState == HIGH){
+    digitalWrite(lamp, LOW);
+    Serial.println("Lamp is OFF");
+    Serial.println("-----");
+  }  
+  else {
+    digitalWrite(lamp, HIGH);
+    Serial.println("Lamp is ON");
+    Serial.println("-----");
+  }
+}
+*/
---
+++ UnoSenderSerial/sketch_jul21aUnoSenderSerial.ino
@@ -0,0 +1,140 @@
+const int YButton = 2;
+const int AButton = 4;
+const int XButton = 5;
+const int BButton = 3;
+const int menuButton = 6;
+const int viewButton = 7;
+const int joystickButton = 8;
+const int joystickAxisX = A0;
+const int joystickAxisY = A1;
+const int buttons[] = {YButton, AButton, XButton, BButton, menuButton, viewButton, joystickButton};
+
+int xValue;
+int yValue;
+
+char* joystickValue;
+char* buttonValue;
+char* menuValue = "2";
+
+bool lampState = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i; i < 7; i++)
+  {
+   pinMode(buttons[i], INPUT);
+   digitalWrite(buttons[i], HIGH);
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.begin(2000000);
+  Serial.setTimeout(1); // gotta go fast
+}
+
+void loop() {
+
+  xRegion();
+  yRegion();
+
+  joystickState();
+  buttonState();
+  menuState();
+  
+//  Serial.print(xValue), Serial.print(" | "), Serial.println(yValue);
+  delay(100);
+
+}
+
+void xRegion() {
+  
+  if (analogRead(joystickAxisX) > 575) {
+    xValue = 1;
+  } else if (analogRead(joystickAxisX) < 447) {
+    xValue = -1;
+  } else {
+    xValue = 0;
+  }
+
+}
+
+void yRegion() {
+  
+  if (analogRead(joystickAxisY) > 575) {
+    yValue = 1;
+  } else if (analogRead(joystickAxisY) < 447) {
+    yValue = -1;
+  } else {
+    yValue = 0;
+  }
+
+}
+
+void joystickState() {
+  if (xValue == -1 && yValue == 1) {
+    joystickValue = "q";
+  } else if (xValue == 0 && yValue == 1) {
+    joystickValue = "w";
+  } else if (xValue == 1 && yValue == 1) {
+    joystickValue = "e";
+  } else if (xValue == -1 && yValue == 0) {
+    joystickValue = "a";
+  } else if (xValue == 1 && yValue == 0) {
+    joystickValue = "d";
+  } else if (xValue == -1 && yValue == -1) {
+    joystickValue = "z";
+  } else if (xValue == 0 && yValue == -1) {
+    joystickValue = "x";
+  } else if (xValue == 1 && yValue == -1) {
+    joystickValue = "c";
+  } else {
+    joystickValue = "s";
+  }
+
+  Serial.print(joystickValue);
+  
+}
+
+void buttonState() {
+  if (digitalRead(YButton) == 0) {
+    buttonValue = "i";
+  } else if (digitalRead(XButton) == 0) {
+    buttonValue = "j";
+  } else if (digitalRead(AButton) == 0) {
+    buttonValue = ",";
+  } else if (digitalRead(BButton) == 0) {
+    buttonValue = "l";
+  } else {
+    buttonValue = "k";
+  }
+
+  Serial.print(buttonValue);
+}
+
+void menuState() {
+  if (digitalRead(menuButton) == 0) {
+    delay(100);
+    if (lampState == LOW) {
+      menuValue = "1";
+      lampState = HIGH;
+    } else {
+      menuValue = "2";
+      lampState = LOW;
+    }
+  }
+
+  Serial.println(menuValue);
+}
+
+void serialDebug() {
+  Serial.print("Y = "),Serial.print(digitalRead(YButton)),Serial.print(" | ");
+  Serial.print("A = "),Serial.print(digitalRead(AButton)),Serial.print(" | ");
+  Serial.print("X = "),Serial.print(digitalRead(XButton)),Serial.print(" | ");
+  Serial.print("B = "),Serial.print(digitalRead(BButton)),Serial.print(" | ");
+  Serial.print("Menu = "),Serial.print(digitalRead(menuButton)),Serial.print(" | ");
+  Serial.print("View = "),Serial.print(digitalRead(viewButton)),Serial.print(" | ");
+  Serial.print("Joystick = "),Serial.print(digitalRead(joystickButton)),Serial.print(" | ");
+  Serial.print("X = "),Serial.print(map(analogRead(joystickAxisX), 0, 1023, 0, 100)), Serial.print("%"), Serial.print(" | ");
+  Serial.print("Y = "),Serial.print(map(analogRead(joystickAxisY), 0, 1023, 0, 100)), Serial.println("%");
+}
+
